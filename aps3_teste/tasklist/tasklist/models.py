eca4b8408970fa3b7f9b3cfab12c95934b7c5d6b
luizasilveira <38442561+luizasilveira@users.noreply.github.com>
2020-11-06 19:13:27 -0300

    APS3

--- aps3_teste/tasklist/tasklist/models.py
+++ aps3_teste/tasklist/tasklist/models.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
 
-
 # pylint: disable=too-few-public-methods
 class Task(BaseModel):
     description: Optional[str] = Field(
@@ -15,15 +14,20 @@ class Task(BaseModel):
         False,
         title='Shows whether the task was completed',
     )
+    user_id : Optional[str] = Field(
+        None,
+        title='User ID',
+    )
 
     class Config:
         schema_extra = {
             'example': {
                 'description': 'Buy baby diapers',
                 'completed': False,
+                'user_id': "user id",
             }
         }
-        
+
 #Create User Model
 class User(BaseModel):
     username: Optional[str] = Field(
